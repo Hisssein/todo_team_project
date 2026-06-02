20101a34995f490739e80932fefb6596f1d2f36b
moustaphaseidissa-blip <moustaphaseidissa@gmail.com>
2026-06-02 22:44:36 +0100

    Ajout fonctionnalites JavaScript

---
+++ script.js
@@ -0,0 +1,35 @@
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", () => {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Veuillez entrer une tâche");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+
+    span.addEventListener("click", () => {
+        span.classList.toggle("completed");
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Supprimer";
+
+    deleteBtn.addEventListener("click", () => {
+        li.remove();
+    });
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+});
